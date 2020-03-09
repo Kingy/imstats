2bfe4cfb5ff76f8f93d72ca0fab56eea505bd3a3
Jamie Gracie <jamie.gracie@takaud.com>
2020-03-09 11:50:08 +0300

    CHANGED: Tidied up race scraper

--- main.py
+++ main.py
@@ -6,46 +6,55 @@ import time
 from bs4 import BeautifulSoup
 import pandas as pandas
 
-url = 'https://www.ironman.com/races'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--blink-settings=imagesEnabled=false')
 
-driver = webdriver.Chrome('driver/chromedriver.exe', options=chrome_options)
-driver.get(url)
-html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-
-raceLinks = []
-
-driver.execute_script("window.scrollTo(0, window.scrollY + 4800)")
-pageCount = len(driver.find_elements_by_xpath("//div[@class='pageButtons']/div[contains(@class, 'pageNumber')]"))
-print(pageCount)
-
-# Do the first page
-for div in soup.find_all('div', {'class' : 'race-details-right'}):
-    for link in div.find_all('a', href=True):
-        raceLinks.append(link['href'])
-
-driver.execute_script("window.scrollTo(0, window.scrollY + 0)")
-
-x = 1
-while x < pageCount: 
-    driver.execute_script("window.scrollTo(0, window.scrollY + 4600)")
-    time.sleep(2)
-    nxt = driver.find_element_by_xpath("//div[@class='paginationButtons']/button[@class='nextPageButton']")
-    actions = ActionChains(driver)
-    actions.click(nxt).perform()
-    time.sleep(2)
-
+def getRaces(url):
+    driver = webdriver.Chrome('driver/chromedriver.exe', options=chrome_options)
+    driver.get(url)
     html = driver.page_source
     soup = BeautifulSoup(html, 'lxml')
+    raceLinks = []
+    driver.execute_script("window.scrollTo(0, window.scrollY + 4800)")
+    pageCount = len(driver.find_elements_by_xpath("//div[@class='pageButtons']/div[contains(@class, 'pageNumber')]"))
+
+    # Do the first page
     for div in soup.find_all('div', {'class' : 'race-details-right'}):
         for link in div.find_all('a', href=True):
             raceLinks.append(link['href'])
 
-    x += 1
+    driver.execute_script("window.scrollTo(0, window.scrollY + 0)")
 
-print(raceLinks)
+    x = 1
+    while x < pageCount: 
+        driver.execute_script("window.scrollTo(0, window.scrollY + 4600)")
+        time.sleep(2)
+        nxt = driver.find_element_by_xpath("//div[@class='paginationButtons']/button[@class='nextPageButton']")
+        actions = ActionChains(driver)
+        actions.click(nxt).perform()
+        time.sleep(2)
 
-driver.quit()
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        for div in soup.find_all('div', {'class' : 'race-details-right'}):
+            for link in div.find_all('a', href=True):
+                raceLinks.append(link['href'])
+
+        x += 1
+
+    driver.quit()
+
+    return raceLinks
+
+def getRaceResults(race):
+    return raceResults
+
+def main():
+    url = 'https://www.ironman.com/races'
+    raceLinks = []
+    raceLinks = getRaces(url)
+    print(raceLinks)    
+
+if __name__== "__main__":
+    main()
