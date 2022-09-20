385abee428517bcbbcac10ef6b47ad7faac7bfb1
Jamie Gracie <jamie@gracie.kiwi>
2022-09-20 15:50:36 +0300

    Update AthleteResult.py

--- classes/AthleteResult.py
+++ classes/AthleteResult.py
@@ -1,16 +1,18 @@
 class AthleteResult:
 
-    def __init__(self, id, race_id, name, country, division, gender, div_rank, gen_rank, ovr_rank, tot_time):
+    def __init__(self, id, race_id, name, country, div_rank, gen_rank, rank, swim_time, bike_time, run_time, time, points):
         self.id = id
         self.race_id = race_id
         self.name = name
         self.country = country
-        self.division = division
-        self.gender = gender
         self.div_rank = div_rank
         self.gen_rank = gen_rank
-        self.ovr_rank = ovr_rank
-        self.tot_time = tot_time
+        self.rank = rank
+        self.swim_time = swim_time
+        self.bike_time = bike_time
+        self.run_time = run_time
+        self.time = time
+        self.points = points
 
     def athleteInfo(self):
         return self.name + " from " + self.country
