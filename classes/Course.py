051b681864b3b00dfd666dfdf16a8c15019dd9e9
Jamie Gracie <jamie@gracie.kiwi>
2022-09-21 15:51:43 +0300

    Update Course.py

--- classes/Course.py
+++ classes/Course.py
@@ -1,10 +1,17 @@
 from urllib.parse import urlparse
 
 class Course:
-    def __init__(self, id, name, location, url):
+    def __init__(self, id, name, location, date, image, swim, bike, run, airTemp, waterTemp, url):
         self.id = id
         self.name = name
         self.location = location
+        self.date = date
+        self.image = image
+        self.swim = swim
+        self.bike = bike
+        self.run = run
+        self.air_temp = airTemp
+        self.water_temp = waterTemp
         self.url = url
         self.url_segment = self.createUrlSegment()
 
